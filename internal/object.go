2404f3e156f9738d340fa71e01aff8fcbcc10d02
missayev <feeerhd.342@gmail.com>
2024-10-16 07:18:15 +0500

    feat: get object

--- internal/object.go
+++ internal/object.go
@@ -34,6 +34,15 @@ func UploadNewObject(w http.ResponseWriter, r *http.Request) {
 		writeXMLError(w, "BadRequest", "Error: bucket name cannot be empty.", http.StatusBadRequest)
 		return
 	}
+	is, err := elementExists("/buckets.csv", bucketName)
+	if err != nil {
+		writeXMLError(w, "InternalServerError", "Error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !is {
+		writeXMLError(w, "BadRequest", "Error: bucket name does not exists$.", http.StatusBadRequest)
+		return
+	}
 	// checking that --dir=path exists
 	if _, err := os.Stat(config.Directory); os.IsNotExist(err) {
 		err = os.Mkdir(config.Directory, 0o755)
@@ -89,4 +98,62 @@ func UploadNewObject(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+// NOT FINISHED XML RESPONSE [434234 bytes of object data]!!!
+func RetrieveObject(w http.ResponseWriter, r *http.Request) {
+	// http errors checking
+	if r.Method != http.MethodGet {
+		writeXMLError(w, "MethodNotAllowed", "Error: only GET command in /get/ url.", http.StatusMethodNotAllowed)
+		return
+	}
+	bucketName := ""
+	for _, v := range r.URL.Path[len("/put/"):] {
+		if v == '/' {
+			break
+		}
+		bucketName += string(v)
+	}
+	if bucketName == "" {
+		writeXMLError(w, "BadRequest", "Error: bucket name cannot be empty.", http.StatusBadRequest)
+		return
+	}
+	is, err := elementExists("/buckets.csv", bucketName)
+	if err != nil {
+		writeXMLError(w, "InternalServerError", "Error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !is {
+		writeXMLError(w, "BadRequest", "Error: bucket name does not exists$.", http.StatusBadRequest)
+		return
+	}
+
+	// checking that --dir=path exists
+	if _, err := os.Stat(config.Directory); os.IsNotExist(err) {
+		err = os.Mkdir(config.Directory, 0o755)
+		if err != nil {
+			writeXMLError(w, "InternalServerError", "Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	// checking that '--dir=' is standard or not
+	if isStandardPackage(config.Directory) {
+		writeXMLError(w, "BadRequest", "Error: directory(--dir=) cannot be one of the used ones.", http.StatusBadRequest)
+		return
+	}
+	objectKey := r.URL.Path[len("/put/"+bucketName)+1:]
+	if objectKey == "" {
+		writeXMLError(w, "BadRequest", "Error: object key cannot be empty.", http.StatusBadRequest)
+		return
+	}
+
+	is, err = elementExists("/"+bucketName+"objects.csv", objectKey)
+	if err != nil {
+		writeXMLError(w, "InternalServerError", "Error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !is {
+		writeXMLError(w, "BadRequest", "Error: object key does not exists$.", http.StatusBadRequest)
+		return
+	}
 }
